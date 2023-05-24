424c303ca30345d83ffbe7a69d6386c9b985023a
kouki nakatsu <k-nakatsu@digeon.co>
2023-05-24 17:52:34 +0900

    set up

--- server/main.py
+++ server/main.py
@@ -1,8 +1,37 @@
+from typing import Union
+from pydantic import BaseModel
 from fastapi import FastAPI
 
 app = FastAPI()
 
 
-@app.get("/")
-async def root() -> dict[str, str]:
-    return {"message": "Hello World"}
+class HelloWorld(BaseModel):
+    message: str
+
+
+class Item(BaseModel):
+    name: str
+    description: Union[str, None] = None
+    price: float
+    tax: Union[float, None] = None
+
+
+@app.get("/hello-world", response_model=HelloWorld)
+async def hello_world() -> HelloWorld:
+    return HelloWorld(message="Hello world")
+
+
+@app.get("/items/{item_id}", response_model=Item)
+# パスパラメータではない関数パラメータを宣言すると、それらは自動的に "クエリ" パラメータとして解釈されます。
+# Unionを持ちることで、オプショナルなクエリを設定できる
+async def read_item(
+    item_id, skip: int = 0, bool: bool = False, option: Union[str, None] = None
+) -> Item:
+    if option is None:
+        return Item(name=item_id + str(skip) + str(bool), price=12.9)
+    return Item(name=item_id + str(skip) + str(bool) + option, price=12.9)
+
+
+@app.post("/items/")
+async def create_item(item: Item) -> Item:
+    return item
